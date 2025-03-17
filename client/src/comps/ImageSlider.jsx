978476efb37cc174411b18c5d4f776259c420b47
niyooshawebsites <niyooshawebsites@gmail.com>
2025-03-17 20:12:09 +0530

    Image Slider v1

---
+++ client/src/comps/ImageSlider.jsx
@@ -0,0 +1,44 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css"; // core swiper slides
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+const ImageSlider = () => {
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 3000, disableOnInteraction: false }}
+      loop={true}
+      className="w-full h-[400px]"
+    >
+      <SwiperSlide>
+        <img
+          src="/images/banner1.jpg"
+          alt="Banner 1"
+          className="w-full h-full object-cover"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="/images/banner2.jpg"
+          alt="Banner 2"
+          className="w-full h-full object-cover"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="/images/banner3.jpg"
+          alt="Banner 3"
+          className="w-full h-full object-cover"
+        />
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default ImageSlider;
